3294c3c9ce35ce19f13f47b315f13bed8ec57762
ulassevinc <80682861+ulassevinc@users.noreply.github.com>
2021-05-05 21:27:48 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,19 @@
+userBirthYear = int( input( 'Please enter your birth year:'))
+
+if userBirthYear in range(1928, 1945):
+    print('You are silent Generation')
+if userBirthYear in range(1946, 1964):
+    print('You are Baby Boomers')
+if userBirthYear  in range(1965, 1980): 
+    print('You are Generation X')
+if userBirthYear  in range(1981, 1996):  
+    print('You are Generation Y')
+if userBirthYear in range(1997, 2012): 
+    print('You are Generation Z')
+if userBirthYear in range(2012, 2021):  
+    print('You are Generation Alpha')
+    
+
+    
+                    
+                    
